10dfc5cdf0384ad6514ac1020c7dac626f405019
SaahBrice <saahbrice98@gmail.com>
2025-12-19 22:27:28 +0100

    add translation of landing page

--- gistme_backend/urls.py
+++ gistme_backend/urls.py
@@ -18,13 +18,43 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.conf.urls.i18n import i18n_patterns
+from django.shortcuts import redirect
+from django.utils.translation import get_language_from_request
 
+
+def language_redirect(request):
+    """Redirect root URL to the appropriate language-prefixed version."""
+    # Check for existing language cookie
+    if 'django_language' in request.COOKIES:
+        lang = request.COOKIES['django_language']
+    else:
+        # Detect from browser Accept-Language header
+        lang = get_language_from_request(request, check_path=False) or 'en'
+    
+    # Ensure it's one of our supported languages
+    if lang not in ['en', 'fr']:
+        lang = 'en'
+    
+    return redirect(f'/{lang}/')
+
+
+# Non-localized URLs (API, admin, webhooks should not have language prefix)
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("api.urls")),
     path("accounts/", include("allauth.urls")),  # Google OAuth URLs
-    path("", include("web.urls")),
+    # Language selection endpoint
+    path('i18n/', include('django.conf.urls.i18n')),
+    # Root redirect based on language preference
+    path('', language_redirect, name='root_redirect'),
 ]
+
+# Localized URLs (all web pages get /en/ or /fr/ prefix)
+urlpatterns += i18n_patterns(
+    path("", include("web.urls")),
+    prefix_default_language=True,  # Show /en/ even for default language
+)
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
